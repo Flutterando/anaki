fcde1b7e610f70052f61eca8b173655a3264bac4
Marcos Roque <devmarcosdanielr@gmail.com>
2025-04-20 19:44:31 -0300

    test: convert named params to dollar util

---
+++ anaki_drivers_adapters/modules/postgres/tests/utils/convert_named_params_to_dollar_test.go
@@ -0,0 +1,32 @@
+package utils_test
+
+import (
+	"reflect"
+	"testing"
+
+	"github.com/flutterando/anaki/anaki_drivers_adapters/modules/postgres/utils"
+)
+
+func TestConvertNamedParams(t *testing.T) {
+	query := "INSERT INTO users (name, age) VALUES (:name, :age)"
+	params := map[string]interface{}{
+		"name": "Marcos",
+		"age":  25,
+	}
+
+	expectedQuery := "INSERT INTO users (name, age, sex) VALUES ($1, $2)"
+	expectedArgs := []interface{}{"Marcos", 25}
+
+	finalQuery, args, err := utils.ConvertNamedParams(query, params)
+	if err != nil {
+		t.Fatalf("expected no error, got %v", err)
+	}
+
+	if finalQuery != expectedQuery {
+		t.Errorf("expected query %q, got %q", expectedQuery, finalQuery)
+	}
+
+	if !reflect.DeepEqual(args, expectedArgs) {
+		t.Errorf("expected args %v, got %v", expectedArgs, args)
+	}
+}
