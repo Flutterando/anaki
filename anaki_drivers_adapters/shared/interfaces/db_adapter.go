cac0172c3df09321b8e5d611b897023b04f07fc1
Marcos Roque <devmarcosdanielr@gmail.com>
2025-04-20 21:16:05 -0300

    feat: add execute db adapter contract

--- anaki_drivers_adapters/shared/interfaces/db_adapter.go
+++ anaki_drivers_adapters/shared/interfaces/db_adapter.go
@@ -6,7 +6,13 @@ import (
 	"github.com/flutterando/anaki/anaki_drivers_adapters/shared/types"
 )
 
+type ExecuteResult struct {
+	Rows         []map[string]interface{}
+	RowsAffected int64
+}
+
 type DBAdapter interface {
 	Connect(ctx context.Context, config types.Config) error
 	Disconnect() error
+	Execute(ctx context.Context, query string, args map[string]interface{}) (*ExecuteResult, error)
 }
